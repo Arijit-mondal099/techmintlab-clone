7ca6e38e5a8c0d511c2448ef7e3df1909f93d84e
Arijit Mondal <arijitm717@gmail.com>
2025-08-12 15:34:12 +0530

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import HeroSection from '../components/HeroSection'
-import PortfolioPage from './PortfolioPage'
-import PackagesSection from '../components/PackagesSection'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import PortfolioPage from "./PortfolioPage";
+import PackagesSection from "../components/PackagesSection";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
-    <div>
-        <Navbar />
-        <HeroSection />
-        <PortfolioPage />
-        <PackagesSection />
-        <Footer />
+    <div className="overflow-hidden" >
+      <Navbar />
+      <HeroSection />
+      <PortfolioPage />
+      <PackagesSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
